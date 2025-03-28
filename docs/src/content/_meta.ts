6cbae4190407516615bcc1d0c79fe94f2e57f181
Adam Pietrasiak <adam@pietrasiak.com>
2025-03-28 19:53:30 +0100

    website

--- docs/src/content/_meta.ts
+++ docs/src/content/_meta.ts
@@ -1,13 +1,3 @@
 export default {
   index: "",
-  "using-styles": "Using Styles",
-  "reusing-styles": "Reusing Styles",
-  "inline-components": "Inline Components",
-  colors: "Colors",
-  frame: "Frame",
-  themes: "Themes",
-  "custom-composers": "Custom Composers",
-  conventions: "Conventions",
-  api: "API",
-  performance: "Performance",
 };
